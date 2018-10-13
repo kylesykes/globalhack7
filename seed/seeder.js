496a0dca147e11246cc90475333a66aca3d2c4aa
farrellw <farrell.will.g@gmail.com>
2018-10-13 02:02:05 -0500

    Add basic user object.

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,4 +1,25 @@
 const mongoose = require("mongoose");
+const User = require("../models/User");
+const Messages = require("../models/Messages");
+const MilestoneTemplates = require("../MilestoneTemplates");
+
+let mileStoneObject = {
+  templateId: "5",
+  name: "Drivesr License",
+  description: "To the DMV and get a drivers license",
+  steps: []
+};
+
+let userObject = {
+  username: "farrellw",
+  phone: "5632107275",
+  mentors: [],
+  mentees: [],
+  milestones: [mileStoneObject],
+  profile: {}
+};
+
+let myUser = new User(userObject);
 
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
