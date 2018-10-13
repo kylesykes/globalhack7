a5817c22fe44f354204fcd2c87fcaf4eb74541c1
Katie Mathews <ktmathews89@gmail.com>
2018-10-13 02:01:21 -0500

    add user and milestone template models

---
+++ models/MilestoneTemplate.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const milestoneTemplateSchema = new mongoose.Schema({
+  name: { type: String, unique: true },
+  description: String,
+  steps: {
+    name: String,
+    description: String,
+  }
+}, { timestamps: true });
+
+const MilestoneTemplate = mongoose.model('MilestoneTemplate', milestoneTemplateSchema);
+
+module.exports = MilestoneTemplate;
