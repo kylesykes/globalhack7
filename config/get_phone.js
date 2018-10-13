eeac853ac6879f16dfe6de517555f3ffbfe6d083
farrellw <farrell.will.g@gmail.com>
2018-10-13 12:58:38 -0500

    Change to phoneRegex.

--- config/get_phone.js
+++ config/get_phone.js
@@ -1,6 +1,5 @@
-const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/gm;
+const phoneReg = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/gm;
 module.exports = phone => {
-  var found = phone.match(regex);
-  console.log(found);
+  var found = phone.match(phoneReg);
   return found;
 };
